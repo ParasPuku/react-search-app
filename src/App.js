a20aae6d5351e78ed5129c38fbe7327ae2935ea0
CORP\pamahto <parashnath-kumar.mahto@capgemini.com>
2022-04-29 06:46:36 +0530

    React Search Filter App Completion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
+import "./App.css";
+import Data from "./data.json";
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
 
 function App() {
+  const [filteredComment, setFilteredComment] = useState([]);
+  const [clearInputBox, setClearInputBox] = useState("");
+  const [scroll, setScroll] = useState("");
+
+  useEffect(() => {
+    if (filteredComment.length > 0 && clearInputBox.length > 0) {
+      const searchResult = document.querySelector(".comment").clientHeight;
+      if (searchResult > 200) {
+        setScroll("setScroll");
+      } else {
+        setScroll("");
+      }
+    }
+  }, [filteredComment]);
+
+  const handleInputChange = (event) => {
+    const searchWord = event.target.value;
+    const comment = Data.comments.filter((comment) => {
+      return comment.body.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    setFilteredComment(comment);
+    setClearInputBox(searchWord);
+  };
+
+  const handleOnClose = (event) => {
+    setFilteredComment(0);
+    setClearInputBox("");
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">React Search Filter App</h1>
+        <div className="SearchArea">
+          <input
+            type="text"
+            onChange={handleInputChange}
+            className="SearchBox"
+            value={clearInputBox}
+            placeholder="Search your comments"
+          />
+          <div className="Search-Icon">
+            {filteredComment.length > 0 && clearInputBox.length > 0 ? (
+              <CloseIcon onClick={handleOnClose} />
+            ) : (
+              <SearchIcon />
+            )}
+          </div>
+        </div>
+        {filteredComment.length > 0 && clearInputBox.length > 0 && (
+          <ul
+            className={`comment ${
+              scroll === "setScroll" ? "setScrollHeight" : ""
+            }`}
+          >
+            {filteredComment.map((comment) => (
+              <li
+                key={comment.id}
+                id={`id-${comment.id}`}
+                className="CommentList"
+              >
+                {comment.body}
+              </li>
+            ))}
+          </ul>
+        )}
       </header>
     </div>
   );
